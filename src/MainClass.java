eb76b9757b6546ab2eff55f501238c5d490e23cb
hs <hs@UMC-03456662441>
2021-05-12 19:28:22 +0100

    First Commit

---
+++ src/MainClass.java
@@ -0,0 +1,11 @@
+
+public class MainClass {
+   public static void main(String[] args) {
+	   String hungary="hungary";
+	   if(hungary.equals("hungary")) {
+		   System.out.println("Go and Eat Some Food");   
+	   }else {
+		   System.out.println("Do Work ");
+	   }
+   }
+}
